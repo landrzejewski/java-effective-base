4d01f2ae59a053a5e290157b82542e27aed23d36
student-070 <landrzejewski.poczta@gmail.com>
2026-06-15 11:53:17 +0200

    update

--- src/main/java/pl/training/functionalfeatures/StreamExamples.java
+++ src/main/java/pl/training/functionalfeatures/StreamExamples.java
@@ -1,6 +1,14 @@
 package pl.training.functionalfeatures;
 
+import java.net.SocketTimeoutException;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.*;
 
 public class StreamExamples {
 
@@ -39,8 +47,65 @@ public class StreamExamples {
         );
     }
 
+    static Predicate<Employee> byMinSalary(double minSalary) {
+        return employee -> employee.salary >= minSalary;
+    }
+
+    static String toNames(List<Employee> employees) {
+        return employees.stream()
+                .map(Employee::name)
+                .collect(joining(", "));
+    }
+
     public static void main(String[] args) {
         var employees = createEmployees();
+
+        // employees.forEach(employee ->System.out.println(employee.name()));
+
+        record NameSalary(String name, double salary) {}
+
+        employees.stream()
+                //.filter(employee -> employee.salary >= 100_000)
+                .filter(byMinSalary(100_000))
+                //.map(employee ->  employee.name())
+                //.map(Employee::name)
+                .map(employee -> new NameSalary(employee.name, employee.salary))
+                .forEach(System.out::println);
+
+        employees.stream()
+                .filter(Employee::remote)
+                .findFirst()
+                .ifPresent(System.out::println);
+
+
+        var youngest = employees.stream()
+                .min(comparingInt(Employee::age));
+        var oldest = employees.stream()
+                .max(comparingInt(Employee::age));
+
+        System.out.format("Wiek %d - %d%n",
+                youngest.map(Employee::age).orElse(0),
+                oldest.map(Employee::age).orElse(0)
+        );
+
+        var stats = employees.stream()
+                .mapToDouble(Employee::salary)
+                .summaryStatistics();
+        System.out.format("Pensja min: $%.0f - max: $%.0f (avg. $%.2f)\n", stats.getMin(),  stats.getMax(), stats.getAverage());
+
+
+        double avg = employees.stream().collect(teeing(
+                summingDouble(Employee::salary),
+                counting(),
+                (sum, count) -> count == 0 ? 0 : sum / count
+        ));
+        System.out.println("Average salary: " + avg);
+
+        // employees by dept
+        employees.stream()
+                .collect(groupingBy(Employee::department))
+                .forEach((name, emp) -> System.out.printf("%s: %s\n", name, toNames(emp)));
+
     }
 
 }
